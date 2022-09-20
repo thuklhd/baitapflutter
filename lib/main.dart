720ce2e368a428985b4d978e47c74c18210acd29
thuklhd <97714679+thuklhd@users.noreply.github.com>
2022-09-20 23:18:14 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,249 +1,191 @@
 
-
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
-void main(){
-  runApp(
-   Caculator()
-  );
-}
-class Caculator extends StatefulWidget{
 
+void main() {
+  runApp(Caculator());
+}
+
+class Caculator extends StatefulWidget {
   @override
   State<Caculator> createState() => _CaculatorState();
 }
 
 class _CaculatorState extends State<Caculator> {
-  String  Result="";
-  int FirstNumber=0;
-  int SecondNumber=0;
-  String TheObjectIsDisplayedOnTheScreen="";
-  String Operator="";
-  int CheckWhatNumberYouAreEntering=0;
-  String shadow="";
+  String Result = "";
+  int FirstNumber = 0;
+  int SecondNumber = 0;
+  String TheObjectIsDisplayedOnTheScreen = "";
+  String Operator = "";
+  int CheckWhatNumberYouAreEntering = 0;
+  String ShadowOfResult = "";
 
-  Widget CreateButtom (String Button)
-  {
-    return  Expanded(
-
+  Widget CreateButtom(String Button) {
+    return Expanded(
       child: OutlinedButton(
-
         onPressed: () {
           ClickButton(Button);
         },
-
         child: Text(
           Button,
-          style: TextStyle(
-              fontSize: 20
-          ),
+          style: TextStyle(fontSize: 20),
         ),
-
       ),
     );
   }
-  ClickButton(Button)
-  {
-    if(Button=="c")
+
+  ClickButton(Button) {
+    if (Button == "c") {
+      Result = "";
+      FirstNumber = 0;
+      SecondNumber = 0;
+      TheObjectIsDisplayedOnTheScreen = "";
+      Operator = "";
+    } else if (Button == "+" ||
+        Button == "-" ||
+        Button == "X" ||
+        Button == "/") {
+      Operator = Button;
+      Result = Button;
+    } else if (Button == "=") {
       {
-        Result="";
-        FirstNumber=0;
-        SecondNumber=0;
-        TheObjectIsDisplayedOnTheScreen="";
-        Operator="";
-      }
-else if(Button=="+"||Button=="-"||Button=="X"||Button=="/")
-  {
-    Operator=Button;
-    Result=Button;
+        if (Operator == "+") {
+          Result = (FirstNumber + SecondNumber).toString();
+          FirstNumber = 0;
+          SecondNumber = 0;
+          Operator = "";
+          CheckWhatNumberYouAreEntering = 1;
+        } else if (Operator == "-") {
+          Result = (FirstNumber - SecondNumber).toString();
+          FirstNumber = 0;
+          SecondNumber = 0;
+          Operator = "";
+          CheckWhatNumberYouAreEntering = 1;
+        } else if (Operator == "X") {
+          Result = (FirstNumber * SecondNumber).toString();
+          FirstNumber = 0;
+          SecondNumber = 0;
+          Operator = "";
+          CheckWhatNumberYouAreEntering = 1;
+        } else if (Operator == "/") {
+          double Division = 0.0;
 
-  }
-else if(Button=="="){
+          Division = (FirstNumber / SecondNumber);
+          FirstNumber = 0;
+          SecondNumber = 0;
+          Result = Division.toString();
 
-    {
-
- if(Operator=="+")
-   {
-     Result=(FirstNumber+SecondNumber).toString();
-     FirstNumber=0;
-     SecondNumber=0;
-     Operator="";
-     CheckWhatNumberYouAreEntering=1;
-   }
- else if(Operator=="-")
- {
-   Result=(FirstNumber-SecondNumber).toString();
-   FirstNumber=0;
-   SecondNumber=0;
-   Operator="";
-   CheckWhatNumberYouAreEntering=1;
-
- }
- else if(Operator=="X")
- {
-   Result=(FirstNumber*SecondNumber).toString();
-   FirstNumber=0;
-   SecondNumber=0;
-   Operator="";
-   CheckWhatNumberYouAreEntering=1;
- }
- else if(Operator=="/")
- {
-   double chia=0.0;
-
-   chia=(FirstNumber/SecondNumber);
-   FirstNumber=0;
-   SecondNumber=0;
-   Result=chia.toString();
-
-   Operator="";
-   CheckWhatNumberYouAreEntering=1;
- }
-shadow=Result;
-    }
-  }
-else{
-  if(Operator=="") {
-    if(CheckWhatNumberYouAreEntering==1)
-      {
-
-        FirstNumber = int.parse( Button);
-        CheckWhatNumberYouAreEntering=0;
-        Result=FirstNumber.toString();
-      }
-    else
-      {
-        FirstNumber = int.parse(TheObjectIsDisplayedOnTheScreen + Button);
-        Result=FirstNumber.toString();
-
-      }
-  
-    }
-
-
-  else
-    {
-      if(TheObjectIsDisplayedOnTheScreen=="+"||TheObjectIsDisplayedOnTheScreen=="-"||TheObjectIsDisplayedOnTheScreen=="X"||TheObjectIsDisplayedOnTheScreen=="/")
-        {
-          TheObjectIsDisplayedOnTheScreen="";
+          Operator = "";
+          CheckWhatNumberYouAreEntering = 1;
         }
-      SecondNumber=int.parse(TheObjectIsDisplayedOnTheScreen + Button);
-      Result=SecondNumber.toString();
-
+        ShadowOfResult = Result;
+      }
+    } else {
+      if (Operator == "") {
+        if (CheckWhatNumberYouAreEntering == 1) {
+          FirstNumber = int.parse(Button);
+          CheckWhatNumberYouAreEntering = 0;
+          Result = FirstNumber.toString();
+        } else {
+          FirstNumber = int.parse(TheObjectIsDisplayedOnTheScreen + Button);
+          Result = FirstNumber.toString();
+        }
+      } else {
+        if (TheObjectIsDisplayedOnTheScreen == "+" ||
+            TheObjectIsDisplayedOnTheScreen == "-" ||
+            TheObjectIsDisplayedOnTheScreen == "X" ||
+            TheObjectIsDisplayedOnTheScreen == "/") {
+          TheObjectIsDisplayedOnTheScreen = "";
+        }
+        SecondNumber = int.parse(TheObjectIsDisplayedOnTheScreen + Button);
+        Result = SecondNumber.toString();
+      }
     }
- 
-    }
-setState(() {
-  TheObjectIsDisplayedOnTheScreen=Result;
-});
+    setState(() {
+      TheObjectIsDisplayedOnTheScreen = Result;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-
     // TODO: implement build
-  return    MaterialApp(
-    home: Scaffold
-      (
-      appBar: AppBar(
-        title: Text("may tinh bo tui"),
-
-      ),
-body: Container(
-  child: Column(
-    children: [
-      Container(
-        color: Colors.cyan,
-        child: Row(
-          
-          children: [
-            Expanded(
-
-              child: Container(
-
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("may tinh bo tui"),
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Container(
                 color: Colors.cyan,
-                  alignment:  Alignment.topLeft,
-                  child:OutlinedButton(
-                    onPressed: (){
-                      FirstNumber=int.parse(shadow);
-                      Result=shadow;
-                      setState(() {
-                        TheObjectIsDisplayedOnTheScreen=Result;
-                      });
-                    },
-                    child: Text("history reslut  "+shadow),
-
-                  )
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                          color: Colors.cyan,
+                          alignment: Alignment.topLeft,
+                          child: OutlinedButton(
+                            onPressed: () {
+                              FirstNumber = int.parse(ShadowOfResult);
+                              Result = ShadowOfResult;
+                              setState(() {
+                                TheObjectIsDisplayedOnTheScreen = Result;
+                              });
+                            },
+                            child: Text("history reslut  " + ShadowOfResult),
+                          )),
+                    )
+                  ],
+                ),
               ),
-            )
-          ],
+              Expanded(
+                  child: Container(
+                color: Colors.cyan,
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  TheObjectIsDisplayedOnTheScreen,
+                  style: TextStyle(color: Colors.red, fontSize: 60),
+                ),
+              )),
+              Row(
+                children: [
+                  CreateButtom("c"),
+                  CreateButtom("="),
+                  CreateButtom("0"),
+                  CreateButtom("/")
+                ],
+              ),
+              Row(
+                children: [
+                  CreateButtom("9"),
+                  CreateButtom("8"),
+                  CreateButtom("7"),
+                  CreateButtom("X")
+                ],
+              ),
+              Row(
+                children: [
+                  CreateButtom("6"),
+                  CreateButtom("5"),
+                  CreateButtom("4"),
+                  CreateButtom("-")
+                ],
+              ),
+              Row(
+                children: [
+                  CreateButtom("3"),
+                  CreateButtom("2"),
+                  CreateButtom("1"),
+                  CreateButtom("+")
+                ],
+              )
+            ],
+          ),
         ),
       ),
-      Expanded(
-          child: Container(
-            color: Colors.cyan,
-            alignment: Alignment.bottomRight,
-            child: Text(TheObjectIsDisplayedOnTheScreen,
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 60
-              ),
-            ),
-          )
-      ),
-
-      Row(
-        children: [
-          CreateButtom("c"),
-          CreateButtom("="),
-          CreateButtom("0"),
-          CreateButtom("/")
-
-
-        ],
-      ),
-      Row(
-        children: [
-          CreateButtom("9"),
-          CreateButtom("8"),
-          CreateButtom("7"),
-          CreateButtom("X")
-
-
-        ],
-      ),
-      Row(
-        children: [
-          CreateButtom("6"),
-          CreateButtom("5"),
-          CreateButtom("4"),
-          CreateButtom("-")
-
-
-        ],
-      ),
-      Row(
-        children: [
-          CreateButtom("3"),
-          CreateButtom("2"),
-          CreateButtom("1"),
-          CreateButtom("+")
-
-
-        ],
-      )
-
-    ],
-  ),
-),
-      ),
-
     );
-
   }
 }
-
 
 
 
